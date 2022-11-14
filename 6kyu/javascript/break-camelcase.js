b33d93603a437dc2487396643f292e567b2229bb
Leon <leonmohan.business@gmail.com>
2022-11-13 19:23:50 -0500

    Someone used  in their regex to get the captial char

---
+++ 6kyu/javascript/break-camelcase.js
@@ -0,0 +1,18 @@
+// Complete the solution so that the function will break up camel casing, using a space between words.
+
+// Example
+// "camelCasing"  =>  "camel Casing"
+// "identifier"   =>  "identifier"
+// ""             =>  ""
+
+function solution(string)
+{
+    return string.split("").map((char)=>{
+      if(char.match(/[A-Z]/g))
+      {
+        return " "+char
+      }
+      
+      return char
+    }).join("")
+}

dff7ef2067e36a1057bd80ebf2079a685c912311
unknown <leonmohan.business@gmail.com>
2022-11-30 01:42:23 -0500

    Very close to the top solution! However I didn't know you can acess a list of arguments by just referencing .arguments in the functions body.

---
+++ 7kyu/javascript/how-many-arguments.js
@@ -0,0 +1,10 @@
+// Create a function called args_count that returns the number of arguments provided
+
+//Example:
+// args_count(1, 2, 3) -> 3
+// args_count(1, 2, 3, 10) -> 4
+
+
+function args_count(...args){
+    return args.length
+  }
